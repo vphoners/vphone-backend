f4070867fc4d688fadceccfa69e4040efff73f1f
mohsen <m.hariri@gmail.com>
2016-06-25 03:18:56 +0200

    remove extra debugging

--- vphone-asterisk/entrypoint.sh
+++ vphone-asterisk/entrypoint.sh
@@ -5,7 +5,4 @@ sed -i "s/%USERNAME%/$SIP_USERNAME/g" /etc/asterisk/sip.conf
 sed -i "s/%USERSECRET%/$USER_SECRET/g" /etc/asterisk/sip.conf
 
 
-/usr/sbin/asterisk -f -d &
-sleep 3
-asterisk -vvvvvr
-#/bin/bash
+/usr/sbin/asterisk -f -d
