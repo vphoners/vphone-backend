3f5eec8bf04c41d0b7ebc9ab285f5f3f1ec8c4ef
mohsen <m.hariri@gmail.com>
2016-06-25 15:23:04 +0200

    a few late changes for calls to go through

--- vphone-asterisk/entrypoint_debug.sh
+++ vphone-asterisk/entrypoint_debug.sh
@@ -3,9 +3,8 @@
 sed -i "s/%PASSWORD%/$SIP_PASSWORD/g" /etc/asterisk/sip.conf
 sed -i "s/%USERNAME%/$SIP_USERNAME/g" /etc/asterisk/sip.conf
 sed -i "s/%USERSECRET%/$USER_SECRET/g" /etc/asterisk/sip.conf
-
+sed -i "s/%EXTERN_ADDR%/$EXTERN_ADDR/g" /etc/asterisk/sip.conf
 
 /usr/sbin/asterisk -f -d &
 sleep 3
 asterisk -vvvvvr
-#/bin/bash
