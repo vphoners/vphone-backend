e23b2ca0af1f0c6a69d0f9c67c96a95b1c3ee93c
Mohsen Hariri <m.hariri@gmail.com>
2016-07-08 10:01:53 +0200

    Fix typo in user secret

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 sed -i "s/%PASSWORD%/$SIP_PASSWORD/g" /etc/asterisk/sip.conf
 sed -i "s/%USERNAME%/$SIP_USERNAME/g" /etc/asterisk/sip.conf
-sed -i "s/%USERSECRET%/$USER_SECRET/g" /etc/asterisk/sip.conf
+sed -i "s/%USER_SECRET%/$USER_SECRET/g" /etc/asterisk/sip.conf
 sed -i "s/%EXTERN_ADDR%/$EXTERN_ADDR/g" /etc/asterisk/sip.conf
 
 
